8c94a3fb079b26b01db3641858bb64df176c51f6
yunwei37 <1067852565@qq.com>
2020-10-15 06:47:43 -0700

    fix shut down

--- lab/src/main.rs
+++ lab/src/main.rs
@@ -2,27 +2,39 @@
 #![no_main]
 #![feature(llvm_asm)]
 #![feature(global_asm)]
+#![feature(panic_info_message)]
+#![feature(fmt_as_str)]
 
 global_asm!(include_str!("head.S"));
 
 #[no_mangle]
 pub extern "C" fn start_kernel() -> ! {
-    put_char("Hello RISC-V!\n");
-    loop {}
+    put_chars("Hello RISC-V!\n");
+    panic!("exit..\n");
 }
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    put_chars(info.message().unwrap().as_str().unwrap());
+    shut_down();
     loop {}
 }
 
-fn put_char(msg: &str){
-    let addr = 0x10000000 as *mut u8;
+fn shut_down() {
+    const SIFIVE_TEST: u32 = 0x100000;
+    const VIRT_TEST_FINISHER_PASS: u32 = 0x5555;
+    unsafe {
+        llvm_asm!("sh $0, 0($1)"::"r"(VIRT_TEST_FINISHER_PASS),"r"(SIFIVE_TEST):);
+    }
+}
+
+fn put_chars(msg: &str) {
+    const UART16550A_DR: *mut u8 = 0x10000000 as *mut u8;
     for s in msg.as_bytes() {
         unsafe {
-            *addr = s.clone();
+            *UART16550A_DR = s.clone();
         }
     }
 }
